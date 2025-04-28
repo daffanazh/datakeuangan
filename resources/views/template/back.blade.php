23163255ea34741a36c3ab94dead6bde60e77cd0
daffanazh <daffanazhmi1@gmail.com>
2025-04-28 07:48:18 +0700

    yess

--- resources/views/template/back.blade.php
+++ resources/views/template/back.blade.php
@@ -26,8 +26,7 @@
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet"
     />
-
-    <!-- Icons. Uncomment required icon fonts -->
+    
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0') }}/assets/vendor/fonts/boxicons.css" />
 
     <!-- Core CSS -->
